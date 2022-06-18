4dd27d7779b49216dc69a443b38a72a3df61f977
JFMai90 <jasonmaif@gmail.com>
2022-06-17 22:02:43 -0500

    format for amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {id}</p>
+    <p>
+      {numeral(amount).format('$0,0.00')}
+      -
+      {id}
+    </p>
   </div>
 );
 
